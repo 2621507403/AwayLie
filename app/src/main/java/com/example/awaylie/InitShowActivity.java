0da4632feb870bcd47a7f489a193752ccf00f8dd
2621507403 <2621507403@qq.com>
2023-06-09 20:52:18 +0800

    第二次提交，初始化界面完成

--- app/src/main/java/com/example/awaylie/InitShowActivity.java
+++ app/src/main/java/com/example/awaylie/InitShowActivity.java
@@ -2,7 +2,15 @@ package com.example.awaylie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
@@ -10,11 +18,62 @@ import android.widget.ImageView;
  * **/
 public class InitShowActivity extends AppCompatActivity {
     private ImageView initShowIV;
+    private Button skipShowPic;
+    private Handler handlerThreeSecond;
+    final long threeSecond = 3000;//设置总的倒计时时间
+    final long countInterval = 1000;//倒计时的间隔时间
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_show);
+
         initShowIV = findViewById(R.id.init_show_pic);
-        initShowIV.setImageResource(R.drawable.ic_launcher_background);
+        initShowIV.setImageResource(R.drawable.init_show_pic);
+        skipShowPic = findViewById(R.id.skipShowPic);
+
+        /**加载界面跳过按钮**/
+//        skipShowPic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("InitShowActivity", "Handler postDelayed run");
+//                nextPage(InitShowActivity.this,MainActivity.class);
+//            }
+//        });
+
+        /**
+         * 实现了默认三秒后跳转
+         * */
+        handlerThreeSecond = new Handler(Looper.getMainLooper());
+        handlerThreeSecond.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                nextPage(InitShowActivity.this,MainActivity.class);
+            }
+        },3000);
+
+
+        /**
+         * 使用CountDownTimer 类来实现每倒计时一次，更新一次ui操作
+         *两个参数，第一个是总时间，第二个是间隔，内部方法为每次更新都会实现
+         * */
+        new CountDownTimer(threeSecond, countInterval) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //用于更新按钮
+                skipShowPic.setText("跳过"+millisUntilFinished/1000+"s");
+            }
+            @Override
+            public void onFinish() {
+                //用于更新按钮
+                skipShowPic.setText("跳过");
+            }
+        }.start();
+    }
+
+    /**跳转函数*/
+    private void nextPage(Context context,Class className){
+        Intent intent = new Intent( context , className);
+        startActivity(intent);
+        finish();
     }
 }
